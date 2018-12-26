8dccb4cf87bdd05ea0931948c02e101c71b672ee
comjoy91 <comjoy91@gmail.com>
2018-12-26 12:39:20 +0900

    re-constructing code by function: reducing JS FILES

--- js/onLoad.js
+++ js/onLoad.js
@@ -1,11 +1,6 @@
 
 // ---------- BACKGROUND MAP LAYER ----------
 
-var windowWidth, windowHeight, wideRatio = 0;
-windowWidth = $(window).width();
-windowHeight = $(window).height();
-wideRatio = windowWidth / windowHeight;
-
 var mbAttr = '<a href="https://www.maptiler.com/license/maps/" target="_blank">© MapTiler</a> <a href="https://www.openstreetmap.org/copyright" target="_blank">© OpenStreetMap contributors</a>',
 	mbUrl = 'https://maps.tilehosting.com/data/hillshades/{z}/{x}/{y}.png?key=JrAhm6tBG7Y3CCaBBIMe';
 var grayscale_01 = L.tileLayer(mbUrl, {attribution: mbAttr, opacity: 0.6});
@@ -43,128 +38,149 @@ var map_03 = L.map( 'chapter-03-map', {
 var attribution_03 = L.control.attribution({position: 'topright'}).addTo(map_03);
 // L.control.zoom({position: 'topleft'}).addTo(map);
 
-{ // initial map view setting by different browser window width.
-	if ( windowWidth >= 1024 ) {
-		map_01.setView([35.95, 125.5], 7.3);
-		map_03.setView([35.95, 125.5], 7.3);
+var layer_province_border_01 = L.geoJson(provinceGeoJSON, {
+	style: {
+		opacity: 0, 
+		fillOpacity: 0,
+		weight: province_style.weight
+	},
+	smoothFactor: 0, 
+	interactive: false
+}).addTo(map_01);
+var layer_province_border_03 = L.geoJson(provinceGeoJSON, {
+	style: {
+		opacity: 0, 
+		fillOpacity: 0,
+		weight: province_style.weight
+	},
+	smoothFactor: 0, 
+	interactive: false
+}).addTo(map_03);
+
+
+
+
+// ---------- FUNCTIONS AND TRIGGERS ----------
+
+
+var handler_chapter_01_1 = onAboveBottomOfViewport_initial( $("#chapter-01"), 
+														function() {
+															$(".fixed-background").removeClass("visible");
+															$("#chapter-01-background").addClass("visible");
+															console.log("chapter-01");
+														}, 
+														function() {}
+													);
+var handler_chapter_01_2 = onUnderBottomOfViewport( $("#chapter-01"), 
+														function() {
+															$(".fixed-background").removeClass("visible");
+															$("#chapter-01-background").addClass("visible");
+															console.log("chapter-01");
+														}, 
+														function() {}
+													);
+
+var handler_chapter_02_1 = onAboveBottomOfViewport_initial( $("#chapter-02"),  
+														function() {
+															$(".fixed-background").removeClass("visible");
+															$("#chapter-02-background").addClass("visible");
+															console.log("chapter-02");
+														}, 
+														function() {}
+													);
+var handler_chapter_02_2 = onUnderBottomOfViewport( $("#chapter-02"),  
+														function() {
+															$(".fixed-background").removeClass("visible");
+															$("#chapter-02-background").addClass("visible");
+															console.log("chapter-02");
+														}, 
+														function() {}
+													);
+
+var handler_chapter_03_1 = onAboveBottomOfViewport_initial( $("#chapter-03"), 
+														function() {
+															$(".fixed-background").removeClass("visible");
+															$("#chapter-03-background").addClass("visible");
+															$("#layer-names div").removeClass('orange-background').addClass('red-background');
+															console.log("chapter-03");
+														}, 
+														function() {}
+													);
+var handler_chapter_03_2 = onUnderBottomOfViewport( $("#chapter-03"), 
+														function() {
+															$(".fixed-background").removeClass("visible");
+															$("#chapter-03-background").addClass("visible");
+															$("#layer-names div").removeClass('orange-background').addClass('red-background');
+															console.log("chapter-03");
+														}, 
+														function() {}
+													);
+
+
+function resizeMap_chapter_01_03() {
+
+	var windowWidth, windowHeight, wideRatio = 0;
+	windowWidth = $(window).width();
+	windowHeight = $(window).height();
+	wideRatio = windowWidth / windowHeight;
+
+	{ // initial map view setting by different browser window width.
+		if ( windowWidth >= 1024 ) {
+			map_01.setView([35.95, 125.5], 7.3);
+			map_03.setView([35.95, 125.5], 7.3);
+		}
+		else if ( windowWidth >= 768 && wideRatio > 2/1) { // "iPhone X"
+			map_01.setView([36, 125.5], 6);
+			map_03.setView([36, 125.5], 6);
+		}
+		else if ( windowWidth >= 768 ) {
+			map_01.setView([35.95, 127.6], 7.3);
+			map_03.setView([35.95, 127.6], 7.3);
+		}
+		else if ( wideRatio > 1/1 ) { 
+			map_01.setView([36, 125.5], 6);
+			map_03.setView([36, 125.5], 6);
+		}
+		else { 
+			map_01.setView([35.95, 127.6], 6.5);
+			map_03.setView([35.95, 127.6], 6.5);
+		}
 	}
-	else if ( windowWidth >= 768 && wideRatio > 2/1) { // "iPhone X"
-		map_01.setView([36, 125.5], 6);
-		map_03.setView([36, 125.5], 6);
-	}
-	else if ( windowWidth >= 768 ) {
-		map_01.setView([35.95, 127.6], 7.3);
-		map_03.setView([35.95, 127.6], 7.3);
-	}
-	else if ( wideRatio > 1/1 ) { 
-		map_01.setView([36, 125.5], 6);
-		map_03.setView([36, 125.5], 6);
-	}
-	else { 
-		map_01.setView([35.95, 127.6], 6.5);
-		map_03.setView([35.95, 127.6], 6.5);
-	}
+
+	var mapSvgBound_01 = layer_province_border_01.getBounds();
+	var mapSvgNW_01 = map_01.latLngToContainerPoint(mapSvgBound_01.getNorthWest());
+	var mapSvgSE_01 = map_01.latLngToContainerPoint(mapSvgBound_01.getSouthEast());
+	var mapSvgWidth_01 = mapSvgSE_01.x - mapSvgNW_01.x;
+	var mapSvgLeft_01 = mapSvgNW_01.x;
+	var mapSvgTop_01 = mapSvgNW_01.y;
+	$("#chapter-01-svg").width( mapSvgWidth_01 ).css( { top: mapSvgTop_01+'px', left: mapSvgLeft_01+'px' } );
+
+	var mapSvgBound_03 = layer_province_border_03.getBounds();
+	var mapSvgNW_03 = map_03.latLngToContainerPoint(mapSvgBound_03.getNorthWest());
+	var mapSvgSE_03 = map_03.latLngToContainerPoint(mapSvgBound_03.getSouthEast());
+	var mapSvgWidth_03 = mapSvgSE_03.x - mapSvgNW_03.x;
+	var mapSvgLeft_03 = mapSvgNW_03.x;
+	var mapSvgTop_03 = mapSvgNW_03.y;
+	$("#chapter-03-svg").width( mapSvgWidth_03 ).css( { top: mapSvgTop_03+'px', left: mapSvgLeft_03+'px' } );	
 }
 
 
-// ---------- functions for implementing rawData and score into initial geoJSON data.
-
-function data_object_leaflet(_dataJson_district) {
-	var returnObject = {	
-		"hiringRate_300": {
-			"rawData": _dataJson_district.hiringRate_300,
-			"score": _dataJson_district.score_hiringRate_300
-		}, 
-		"hiringRate_1000": {
-			"rawData": _dataJson_district.hiringRate_1000,
-			"score": _dataJson_district.score_hiringRate_1000
-		}, 
-		"mainIndustryPortion": {
-			"rawData": _dataJson_district.mainIndustryPortion,
-			"score": _dataJson_district.score_mainIndustryPortion
-		}, 
-		"rateOf20sInIndustry": {
-			"rawData": _dataJson_district.rateOf20sInIndustry,
-			"score": _dataJson_district.score_rateOf20sInIndustry
-		}, 
-		"industryJobCreation": {
-			"rawData": _dataJson_district.industryJobCreation,
-			"score": _dataJson_district.score_industryJobCreation
-		}, 
-		"incomeRate": {
-			"rawData": _dataJson_district.incomeRate,
-			"score": _dataJson_district.score_incomeRate
-		}, 
-		"R_COSTII": {
-			"rawData": _dataJson_district.R_COSTII,
-			"score": _dataJson_district.score_R_COSTII
-		}, 
-		"expertRate": {
-			"rawData": _dataJson_district.expertRate,
-			"score": _dataJson_district.score_expertRate
-		},
-		"population": _dataJson_district.population, 
-		"mean_age": _dataJson_district.mean_age, 
-		"numWorkers_inDistrict": _dataJson_district.numWorkers_inDistrict, 
-		"mainIndustry_300": _dataJson_district.mainIndustry_300,
-		"factory": _dataJson_district.factory, 
-		"additional_note": _dataJson_district.additional_note
-	};
-
-	return returnObject;
-};
-
-function dataInsertion_leaflet(_featureArray, _dataArray) {
-	for (var i=0; i<_featureArray.features.length; i++) {
-		var prop = _featureArray.features[i].properties;
-		prop.data = [];
-		prop.score_total = 0;
-
-		for (var j=0; j<_dataArray.length; j++) {
-			prop.data.push(data_object_leaflet(_dataArray[j][i]));
-		}
-		
-		if ( prop.data[j-1].hiringRate_300.score > 0 ) {
-			var data = prop.data[j-1];
-			prop.validForResearch = true; // prop.validForResearch -> 연구대상이냐 아니냐(지도에서 회색이냐 아니냐): hiringRate_300 >= 1%
-			prop.exist_300 = true; // prop.exist_300 -> 300인 이상 사업장이 존재하느냐 마느냐: if (prop.exist_300 && prop.validForResearch) hiringRate_300은 0~1% 사이.
-			
-			prop.score_total += data.hiringRate_300.score;
-			prop.score_total += data.hiringRate_1000.score;
-			prop.score_total += data.mainIndustryPortion.score;
-			prop.score_total += data.rateOf20sInIndustry.score;
-			prop.score_total += data.industryJobCreation.score;
-			prop.score_total += data.incomeRate.score;
-			prop.score_total += data.R_COSTII.score;
-			prop.score_total += data.expertRate.score;
-		}
-		else {
-			prop.validForResearch = false;
-
-			if (prop.data[j-1].hiringRate_300.rawData == "0.00%") prop.exist_300 = false;
-			else prop.exist_300 = true;
-		}
-	}
-};
-
-var municipalData = [_dataJSON_2016.municipals];
-// var provinceData = [_dataJSON_2016.provinces];
-dataInsertion_leaflet(municipalGeoJSON, municipalData);
-// dataInsertion(provinceGeoJSON, provinceData);
-
-// ---------- variable which is related to #mapYear_slider
-// var updateScore = function() { updateScore_check(); }; // recent "updateScore" function variable, depends on which #menu_mapNav is selected.
-var year_index = 0; // 2016: 0. It is used in "js/leaflet_script.js"
-
-// ---------- variable which shows selected polygon (municipal layer, province layer).  Mostly used in "js/leaflet_script.js"
-var current_municipal_layer = null;
-var current_province_layer = null;
 
 
+//jQuery
+$(window).on('DOMContentLoaded load orientationchange resize scroll', handler_chapter_01_1)
+			.on('DOMContentLoaded load orientationchange resize scroll', handler_chapter_02_1)
+			.on('DOMContentLoaded load orientationchange resize scroll', handler_chapter_03_1)
+			.on('orientationchange resize scroll', handler_chapter_01_2)
+			.on('orientationchange resize scroll', handler_chapter_02_2)
+			.on('orientationchange resize scroll', handler_chapter_03_2)
+			.on('DOMContentLoaded load', resizeMap_chapter_01_03); // resize & orientationchange event에 대해서는 아래에서 처리.
 
+map_01.on("resize", function(_event) {
+	resizeMap_chapter_01_03();
+});
+map_03.on("resize", function(_event) {
+	resizeMap_chapter_01_03();
+});
 
-
-(function($){
-
-})(jQuery);
 
